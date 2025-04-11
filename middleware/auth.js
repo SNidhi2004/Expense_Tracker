7e2874261ab0612adf200bc55d8d3413fa977602
shreyaponnada <shreyaponnada150@gmail.com>
2025-04-11 23:37:46 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch {
+    res.sendStatus(401);
+  }
+}
+
+module.exports = auth;
